2660f97af28fc3ae8503bc78b953bf22ce0ed803
Vaibhav <vaibhavgupta2980702@gmail.com>
2025-04-28 21:10:22 +0530

    splash screen added

---
+++ app/src/main/java/com/example/securedocs/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.securedocs
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
